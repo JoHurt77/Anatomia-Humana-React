69215f82e074f99cd6b00a340cdc4c5885d10aa2
JoHurt77 <joel.leon.hurtado@gmail.com>
2024-03-06 15:18:11 +0100

    Anatomía humana con descripciones

---
+++ src/Components/BodyPartsInfo.js
@@ -0,0 +1,50 @@
+export const bodyPartsInfo = {
+  "body-model-head":
+    "La cabeza es la parte superior del cuerpo humano que aloja el cerebro, los ojos, los oídos, la nariz y la boca. Es la parte más alta del cuerpo y contiene varios órganos sensoriales esenciales.",
+  "body-model-eyes":
+    "Los ojos son los órganos de la visión y se encuentran en la parte frontal de la cabeza. Son responsables de percibir la luz y enviar señales al cerebro para interpretar la información visual.",
+  "body-model-nose":
+    "La nariz es el órgano principal del sistema respiratorio y del olfato. Está ubicada en la parte central de la cara, por encima de la boca, y se utiliza para respirar y percibir olores.",
+  "body-model-oral_cavity":
+    "La boca es la abertura en la parte inferior de la cabeza que se utiliza para la alimentación y el habla. Contiene la lengua, los dientes y las glándulas salivales, entre otros órganos.",
+  "body-model-ears":
+    "Las orejas son órganos auditivos ubicados a ambos lados de la cabeza, por encima de la mandíbula. Permiten la percepción del sonido y ayudan en el mantenimiento del equilibrio.",
+  "body-model-neck_or_throat":
+    "El cuello es la parte del cuerpo que conecta la cabeza con el tronco. Contiene la tráquea, el esófago y varias estructuras musculares y vasculares. La garganta es la parte interna del cuello que facilita el paso del aire y los alimentos hacia la tráquea y el esófago.",
+  "body-model-chest":
+    "El pecho es la región frontal del torso entre el cuello y el abdomen. Contiene la caja torácica, que protege los órganos internos como el corazón y los pulmones.",
+  "body-model-nape_of_neck":
+    "La nuca es la parte posterior del cuello, justo debajo del cráneo. Está formada por músculos y tejido adiposo que conectan el cráneo con la columna vertebral.",
+  "body-model-back":
+    "La espalda es la región posterior del torso, que se extiende desde el cuello hasta la pelvis. Contiene la columna vertebral y está formada por músculos, huesos y tejido conectivo.",
+  "body-model-upper_arm":
+    "El brazo superior es la porción del miembro superior que va desde el hombro hasta el codo. Contiene huesos como el húmero y músculos como el bíceps y el tríceps.",
+  "body-model-upper_abdomen":
+    "El abdomen superior es la parte del abdomen ubicada entre el pecho y el ombligo. Contiene órganos como el estómago, el hígado y el diafragma.",
+  "body-model-mid_abdomen":
+    "El abdomen medio es la porción del abdomen ubicada entre el ombligo y la parte inferior del abdomen. Contiene órganos como el intestino delgado y el páncreas.",
+  "body-model-lower_abdomen":
+    "El abdomen inferior es la parte del abdomen ubicada debajo del ombligo. Contiene órganos como el intestino grueso, la vejiga y los órganos reproductivos.",
+  "body-model-elbow":
+    "El codo es la articulación que une el brazo superior con el antebrazo. Permite la flexión y la extensión del brazo.",
+  "body-model-lower_back":
+    "La parte baja de la espalda es la región inferior de la espalda, por encima de las nalgas y debajo de la cintura. Es una zona común de dolor debido a la tensión muscular y lesiones.",
+  "body-model-forearm":
+    "El antebrazo es la porción del miembro superior que va desde el codo hasta la muñeca. Contiene huesos como el radio y el cúbito, y músculos como el flexor y el extensor.",
+  "body-model-buttocks":
+    "Los glúteos son los músculos grandes y redondeados ubicados en las nalgas. Son responsables de la estabilidad y el movimiento de la pelvis.",
+  "body-model-anus":
+    "El ano es el orificio terminal del tracto digestivo a través del cual se expulsan los desechos sólidos del cuerpo.",
+  "body-model-sexual_organs":
+    "Los genitales son los órganos reproductores externos e internos del sistema reproductivo. Incluyen estructuras como el pene en los hombres y la vulva en las mujeres.",
+  "body-model-hand":
+    "La mano es la parte distal del miembro superior, compuesta por la palma, los dedos y el dorso. Es una estructura altamente funcional que permite la manipulación y la realización de tareas finas.",
+  "body-model-thigh":
+    "El muslo es la porción del miembro inferior que va desde la cadera hasta la rodilla. Contiene huesos como el fémur y músculos como el cuádriceps y los isquiotibiales.",
+  "body-model-lower_leg":
+    "La pierna inferior es la porción del miembro inferior que va desde la rodilla hasta el tobillo. Contiene huesos como la tibia y el peroné, y músculos como los gemelos y el tibial anterior.",
+  "body-model-knee":
+    "La rodilla es la articulación que une el muslo con la pierna. Permite la flexión y la extensión de la pierna y es una de las articulaciones más grandes y complejas del cuerpo humano.",
+  "body-model-foot":
+    "El pie es la parte distal del miembro inferior, compuesta por el tobillo, el empeine, los dedos y la planta. Es responsable de soportar el peso del cuerpo y facilitar la locomoción.",
+};
